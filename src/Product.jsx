bf69d84fd1a9e2b40330e0bff92365e108a97478
Shashijass <shashi16ic14@gmail.com>
2021-11-29 16:54:57 +0530

    .

--- src/Product.jsx
+++ src/Product.jsx
@@ -4,7 +4,17 @@ import { useEffect, useState } from "react"
 const Products = () => {
 
     const [data, setData] = useState([]);
-    
+    const[filter , setFilter] = useState(data);
+    const [loading, setloading] = useState(false);
+
+    let componentMounted = true;
+    useEffect(() => {
+        const getProducts = async () => {
+            setloading(true);
+            const response = await fetch
+        }
+    getProducts()
+    },[])
     return (
         <div>
 
